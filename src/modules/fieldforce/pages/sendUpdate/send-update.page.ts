46ae501014d97f4a43c8c319e8d8be167eceaa22
Darren Baines <dbaines1@hotmail.com>
2019-09-13 08:40:54 +0100

    Initial FF Version

---
+++ src/modules/fieldforce/pages/sendUpdate/send-update.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'send-update.page.html',
+  styleUrls: ['send-update.page.scss'],
+})
+export class SendUpdatePage {
+
+  constructor() {}
+
+}
