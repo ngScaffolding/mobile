35a414d6b7954c4c847520b9cd195a94a3f11916
Darren Baines <dbaines1@hotmail.com>
2019-11-14 18:24:27 +0000

    Init changes

--- src/custom/custom.init.ts
+++ src/custom/custom.init.ts
@@ -1,7 +1,6 @@
 import { APP_INITIALIZER } from '@angular/core';
 import { dependencies as statusUpdatesDependencies, startPolling } from './statusUpdates.init';
 import { dependencies as workItemUpdatesDependencies, startWorkItemsPolling } from './workItemsUpdates.init';
-import { dependencies as cacheDependencies, warmCache } from './cache.init';
 import { AppSettingsService } from 'ngscaffolding-core';
 import { environment } from '../environments/environment';
 
